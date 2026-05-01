66a65cac3345d242b195bcbe51d79b7b8cf9cada
ynkrpnk <ankarpenko35@gmail.com>
2026-05-01 15:39:05 +0300

    fixed correct version

--- src/index.js
+++ src/index.js
@@ -1,24 +1,36 @@
 "use strict";
 
-const arrTest = ['Y', 'a', 'n'];
+const arrTest = ["Y", "a", "n"];
 
-const myReduceRight = (arr, callback, start) => {
+function myReduceRight(arr, callback, initialValue) {
   let result;
-  if (start) {
-    result = start;
-    for (let i = arr.length - 1; i >= 0 ; i--) {
-      result = callback(result, arr[i], i, arr);
-    }
+  let startIndex;
+  const hasInitial = arguments.length >= 3;
+
+  if (arr.length === 0 && !hasInitial) {
+    console.log("Нічого не передано!");
+  }
+  if (arr.length === 0 && hasInitial) {
+    return initialValue;
+  }
+  if (arr.length === 1 && !hasInitial) {
+    return arr[0];
+  }
+
+  if (hasInitial) {
+    result = initialValue;
+    startIndex = arr.length - 1;
   } else {
     result = arr[arr.length - 1];
-    for (let i = arr.length - 2; i >= 0 ; i--) {
-      result = callback(result, arr[i], i, arr);
-    }
+    startIndex = arr.length - 2;
+  }
+
+  for (let i = startIndex; i >= 0; i--) {
+    result = callback(result, arr[i], i, arr);
   }
   return result;
-};
+}
 
-console.log(myReduceRight(arrTest, (a, b) => {
-    return a + b;
-  }),
-);
+let test = ["Y", "a", "n"];
+
+console.log(myReduceRight(test, (acc, char) => acc + char));
